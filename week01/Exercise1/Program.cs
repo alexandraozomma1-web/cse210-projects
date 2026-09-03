81399119fd82b51f01170c549171f71b45b9d5f9
Adaugo Alexandra Ozomma <alexandraozomma1@gmail.com>
2026-09-03 20:29:09 +0100

    Updated Exercise 1: Added program to ask user for first and last name, then display formatted output

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-    
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        Console.Write("What is your last name? A");
+        Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
+    
