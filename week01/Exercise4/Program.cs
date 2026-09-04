6e29c9166c45745a056b3deb0a303e9a434dc8a2
Adaugo Alexandra Ozomma <alexandraozomma1@gmail.com>
2026-09-04 13:02:19 +0100

    -Implemented user input loop to collect numbers until 0 is entered - Calculated sum, average, and maximum values from the list - Added stretch challenge features:    • that finds the smallest positive number (closest to zero)    • Sorts the list in ascending order and display results - Ensured 0 is not added to the list to keep calculations accurate

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+           {
+            max = num;
+            }
+        }
+            Console.WriteLine($"The maximum number is: {max}");
+        
+        int smallestPositive = int.MaxValue;
+        foreach (int num in numbers)
+        {
+            if (num > 0 && num < smallestPositive)
+            {
+                smallestPositive = num;
+            }
+        }
+        if (smallestPositive != int.MaxValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There are no positive numbers.");
+        }
+
+        numbers.Sort();
+        Console.WriteLine("The numbers in ascending order are:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+
     }
 }
