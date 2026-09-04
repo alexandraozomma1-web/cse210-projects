35541e3a5ba226d3ca90e5996ddb7e00e1c02f07
Adaugo Alexandra Ozomma <alexandraozomma1@gmail.com>
2026-09-04 12:02:28 +0100

    Added random number guessing game with replay - Implemented loop to keep guessing until correct - Added guess counter to track number of attempts - Displayed total guesses at end of each round - Prompted user to play again and restart game if 'yes'

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,76 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("What is the magic number? ");
+        // string input = Console.ReadLine();
+        // int magicNumber = int.Parse(input);
+
+        // int guess = -1;
+
+        // while (guess != magicNumber)
+        // {
+
+        //     Console.Write("What is your guess? ");
+        //     string guessInput = Console.ReadLine();
+        //     guess = int.Parse(guessInput);
+
+        //     if (guess < magicNumber)
+        //     {
+        //         Console.WriteLine("Guess is too low. Try again.");
+        //     }
+
+        //     else if (guess > magicNumber)
+        //     {
+        //         Console.WriteLine("Guess is too high. Try again.");
+        //     }
+
+        //     else if (guess == magicNumber)
+        //     {
+        //         Console.WriteLine("You guessed it correctly! Congratulations!");
+        //     }
+        // }
+
+        string playAgain = "yes";
+        while (playAgain.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+    
+        while (guess !=magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+            guessCount++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Guess is too low. Try again.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Guess is too high. Try again.");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it correctly in {guessCount} tries! Congratulations!");
+            }
+        }
+        Console.Write("Do you want to play again? (yes/no) ");
+        playAgain = Console.ReadLine();
+        }
+        
+        Console.WriteLine("Thank you for playing! Goodbye!");
+
     }
+
 }
+
+
